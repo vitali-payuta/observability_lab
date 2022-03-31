970a5235d8adb632f9203885320dd7e3ec0bdbfe
Vitali Payuta <vitali_payuta@epam.com>
2022-03-31 15:54:10 +0300

    ht1 ssh l/p sh file

--- hometask1/client.sh
+++ hometask1/client.sh
@@ -2,5 +2,6 @@
 sudo yum install -y openldap-clients nss-pam-ldapd
 
 sudo authconfig --enableldap --enableldapauth --ldapserver=192.168.85.11 --ldapbasedn="dc=devopsldab,dc=com" --enablemkhomedir --update
-
+sudo sed -i ' s;PasswordAuthentication no;PasswordAuthentication yes;' /etc/ssh/sshd_config
+sudo systemctl restart sshd
 sudo systemctl restart nslcd
